1cbb6734f4c3134eb4ffdd437531475f907f3fa7
suraj <alphaguy433@gmail.com>
2024-10-15 16:34:56 +0530

    server setup done

---
+++ backend/server.js
@@ -0,0 +1,36 @@
+
+import express from "express"
+import cors from "cors"
+import dotenv from "dotenv"
+import morgan from "morgan"
+import colors from "colors"
+
+
+const app = express()
+dotenv.config()
+
+const port        = process.env.PORT || 8080
+const mode        = process.env.NODE_MODE
+const client_url  = process.env.CLIENT_URL
+
+
+app.listen(port , ()=>{
+        console.log(`Server is running in ${mode} environment at ${port}`.bgCyan.white)
+});
+
+
+app.get("/", (req, res)=>{
+    res.send("<h1>Weather API  Server Backend</h1>")
+});
+const corsOption={
+    origin     : client_url,
+    mmethod    : ['GET',"POST", "PUT", "DELETE"],
+    credentials: true,
+    optionSuccessStatus:200
+};
+app.use(cors(corsOption))
+app.use(express.json())
+app.use(morgan("dev"))
+
+
+
